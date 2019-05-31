104f2f15da10d16cd39f24d655a4767a8fcf14fa
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-05-31 13:00:06 +0900

    OGP

---
+++ resources/views/ogp/index.blade.php
@@ -0,0 +1,20 @@
+
+<!doctype html>
+    <head>
+        <title>ツイッターモンスター</title>
+        <meta property="og:title" content="ツイッターモンスター">
+        <meta property="og:image" content="https://mitsudaman.github.io/ogp.png">
+        <meta property="og:description" content="ツイッターモンスター">
+        <meta property="og:url" content="https://www.choice-maker.site/">
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="ツイッターモンスター">
+        <meta name="twitter:site" content="@mitudama">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="ツイッターモンスター">
+        <meta name="twitter:image" content="https://mitsudaman.github.io/ogp.png">
+        <meta name="twitter:description" content="ツイッターモンスター">
+    </head>
+    <body>
+        <!-- <script type="text/javascript">window.location="/read/?d=${doc_id}";</script> -->
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/og/{id}', 'OGPController@index')->name('ogp.index');
