ce01b3a08ee7676b48cf6ef08c7dc9c8b1dd1c20
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-07-02 23:00:24 +0900

    使ってないフィールド削除

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'account_id' => $faker->word,
         'name' => $faker->name,
         'title' => $faker->sentence(2),
         'feature1' => $faker->word,
@@ -29,6 +30,6 @@ $factory->define(User::class, function (Faker $faker) {
         // 'email' => $faker->unique()->safeEmail,
         // 'email_verified_at' => now(),
         // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        // 'remember_token' => Str::random(10),
     ];
 });
