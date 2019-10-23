927007ab2cd91d0775d2b7511884a0aa1059dbe0
mitsudaman <k.m.masato-uver-aquarius@hotmail.co.jp>
2019-10-23 17:20:30 +0900

    errorlog

--- config/app.php
+++ config/app.php
@@ -15,6 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'log' => 'errorlog',// 変更する
     /*
     |--------------------------------------------------------------------------
     | Application Environment
